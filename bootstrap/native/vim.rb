8e714af0a67f090274bafb80e316f50636524e4c
Marian Posaceanu <contact@marianposaceanu.com>
2026-09-19 01:01:47 +0300

    feat(bootstrap): add M4-tuned vim Homebrew tap formula
    Copy of homebrew-core vim.rb with bottle block removed, env :std, and
    -O3 -mcpu=apple-m4 cflags. Install via local tap marian/local:
      brew tap-new --no-git marian/local
      cp bootstrap/native/vim.rb $(brew --repo marian/local)/Formula/vim.rb
      brew install --build-from-source marian/local/vim && brew pin vim

---
+++ bootstrap/native/vim.rb
@@ -0,0 +1,112 @@
+class Vim < Formula
+  desc "Vi 'workalike' with many additional features"
+  homepage "https://www.vim.org/"
+  # vim should only be updated every 50 releases on multiples of 50
+  url "https://github.com/vim/vim/archive/refs/tags/v9.2.1100.tar.gz"
+  sha256 "01f855db1f2f61a626eae09e684ee1e585b7d954431d49b29eb0a06632436d72"
+  license "Vim"
+  compatibility_version 1
+  head "https://github.com/vim/vim.git", branch: "master"
+
+  # The Vim repository contains thousands of tags and the `Git` strategy isn't
+  # ideal in this context. This is an exceptional situation, so this checks the
+  # first 50 tags using the GitHub API (to minimize data transfer).
+  livecheck do
+    url "https://api.github.com/repos/vim/vim/tags?per_page=50"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json.map do |tag|
+        match = tag["name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+    throttle 50
+  end
+
+
+  depends_on "gettext" => :build
+  depends_on "lua" => [:build, :test]
+  depends_on "python@3.14" => [:build, :test]
+  depends_on "ruby" => [:build, :test]
+  depends_on "libsodium"
+  depends_on "ncurses"
+
+  uses_from_macos "perl" => [:build, :test]
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "acl"
+  end
+
+  conflicts_with "ex-vi", because: "vim and ex-vi both install bin/ex and bin/view"
+  conflicts_with "macvim", because: "vim and macvim both install vi* binaries"
+  conflicts_with "vim-classic", because: "vim and vim-classic both install vi* binaries"
+
+  def extra_deps = deps.select { |dep| dep.build? && dep.test? }
+
+  deny_network_access!
+
+  env :std
+
+  def install
+    opt_flags = "-O3 -mcpu=apple-m4"
+    ENV.append_to_cflags opt_flags
+    ENV.prepend_path "PATH", formula_opt_libexec("python@3.14")/"bin"
+
+    # Allow dynamically loading formulae libraries when not linked
+    extra_deps.each do |dep|
+      extra_rpath = dep.to_formula.opt_lib
+      extra_rpath = rpath(target: extra_rpath) if OS.mac? # cannot use $ORIGIN
+      ENV.append "LDFLAGS", "-Wl,-rpath,#{extra_rpath}"
+    end
+
+    # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
+    # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
+    # system vimscript files. We specify the normal installation prefix
+    # when calling "make install".
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
+                          "--mandir=#{man}",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--with-compiledby=native-apple-m4",
+                          "--with-modified-by=[ apple-m4 :: #{opt_flags} ]",
+                          "--enable-cscope",
+                          "--enable-terminal",
+                          "--enable-perlinterp#{"=dynamic" unless OS.mac?}",
+                          "--enable-python3interp=dynamic",
+                          "--enable-rubyinterp=dynamic",
+                          "--disable-gui",
+                          "--without-x",
+                          "--enable-luainterp=dynamic",
+                          "--with-lua-prefix=#{formula_opt_prefix("lua")}"
+    system "make"
+    # Parallel install could miss some symlinks
+    # https://github.com/vim/vim/issues/1031
+    ENV.deparallelize
+    system "make", "install", "prefix=#{prefix}"
+    bin.install_symlink "vim" => "vi"
+  end
+
+  def caveats
+    "Additional features can be enabled by installing: #{extra_deps.map(&:name).join(", ")}"
+  end
+
+  test do
+    (testpath/"commands.vim").write <<~VIM
+      :python3 import vim; vim.current.buffer[0] = 'hello python3'
+      :ruby Vim::Buffer.current.append(0, 'hello ruby')
+      :perl $curbuf->Append(0, "hello perl")
+      :lua vim.buffer():insert("hello lua")
+      :wq
+    VIM
+    system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+    assert_equal "hello perl\nhello ruby\nhello python3\nhello lua", File.read("test.txt").chomp
+    assert_match "+gettext", shell_output("#{bin}/vim --version")
+    assert_match "+sodium", shell_output("#{bin}/vim --version")
+  end
+end
